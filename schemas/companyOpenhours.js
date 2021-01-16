5b86ff294699f5cd7346052fb1ea7233824287d7
4Mago <47850942+4Mago@users.noreply.github.com>
2021-01-16 17:45:45 +0100

    Initial commit

---
+++ schemas/companyOpenhours.js
@@ -0,0 +1,17 @@
+export default {
+	title: 'Company Open Hours',
+	name: 'companyOpenhours',
+	type: 'object',
+	fields: [
+		{
+			title: 'Vardagar',
+			name: 'vardagar',
+			type: 'string',
+		},
+		{
+			title: 'Helger',
+			name: 'helger',
+			type: 'string',
+		},
+	],
+}
---
+++ schemas/priskategoriItem.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Kategori",
+  name: "kategori",
+  type: "object",
+  fields: [
+    {
+      title: "Name",
+      name: "name",
+      type: "string",
+    },
+    {
+      title: "Active?",
+      name: "active",
+      type: "boolean",
+    },
+  ],
+}
